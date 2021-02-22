cec7526d7c99f134fc5daf2a00b7b0a1d9170343
Givaldo Ribeiro <givaribeiro23@gmail.com>
2021-02-22 20:04:40 -0300

    Primeiro dia NLW

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,15 @@
+export function ExperienceBar() {
+  return (
+    <header className="experiend-bar">
+      <span>0 px</span>
+        <div className=''>
+          <div style={{width: '50%'}}>
+            <span className="current-experience" style={{left: '50%'}}>
+              300xp
+            </span>
+          </div>
+        </div>
+      <span>600 px</span>
+    </header>
+  );
+}
